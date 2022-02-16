d88fb29a6efbb08c6518ad49703eec7f13be28f9
John Doe <gogs@fake.local>
2022-02-16 15:19:03 +0200

    Добавьте файлы проекта.

---
+++ Inheritance/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Employee:Human
+    {
+        private double _salary;
+        public Employee(string fName,string lName, double salary)
+        {
+            LastName = fName;
+            FirstName = lName;
+            _salary = salary;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Фамилия:{LastName}\n" +
+                $"Имя:{FirstName}\n" +
+                $"Заработная плата:{_salary}");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee("Иван", "Матрасов", 45600);
+            employee.Print();
+        }
+    }
+}
